c49fe607da6d11dfbd6f20588c0b88fe325bc74b
Rakshit rajput <Rakshitrajput212@gmail.com>
2025-04-20 18:21:30 +0530

    Add CORS middleware to allow requests from the frontend

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ const authRouter = require("./routes/authRoutes");
 const profileRouter = require("./routes/profileRoutes");
 const requestRouter = require("./routes/requestsRouter");
 const userRouter = require("./routes/userRouter");
+const cors = require("cors");
 //middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 // Define a port
